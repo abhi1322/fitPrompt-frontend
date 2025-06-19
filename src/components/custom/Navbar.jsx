5eab440047d504bb9c4e2e2aca2a6f4c2574e5a2
unknown <ss2541641@gmail.com>
2025-06-19 15:00:36 +0530

    feat: landing pages changes and nav bar fix

--- src/components/custom/Navbar.jsx
+++ src/components/custom/Navbar.jsx
@@ -7,17 +7,73 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { useLocation } from "react-router-dom";
+import Logo from "../../assets/logo.png";
 
 const Navbar = ({ user }) => {
+  const logout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
+  const location = useLocation();
+
   return (
     <>
       <nav className="w-full flex h-15 justify-between items-center px-36">
         <div>
           <h1>
-            <a href="/">fitprompt</a>
+            <a href="/">
+              <img src={Logo} alt="logo" className="w-30" />
+            </a>
           </h1>
         </div>
-        <div className="flex gap-4">
+
+        <div className="flex gap-2">
+          {!user ? (
+            <>
+              <Button
+                className="rounded-full "
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </Button>
+              <Button
+                className="rounded-full bg-neutral-800 text-white px-6 text-sm"
+                onClick={() => navigate("/register")}
+              >
+                Sign Up
+              </Button>
+            </>
+          ) : (
+            <>
+              {location.pathname != "/dashboard" ? (
+                <Button className="rounded-full px-6 text-sm">
+                  <a href="/dashboard">Dashboard</a>
+                </Button>
+              ) : null}
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <p className="border w-10 h-10 flex justify-center items-center rounded-full cursor-pointer">
+                    {user?.firstName?.[0]}
+                    {user?.lastName?.[0]}
+                  </p>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>Settings</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Edit Profile</DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => logout()}>
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          )}
+        </div>
+        {/* <div className="flex gap-4">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <p className="border w-10 h-10 flex justify-center items-center rounded-full cursor-pointer">
@@ -34,7 +90,7 @@ const Navbar = ({ user }) => {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-        </div>
+        </div> */}
       </nav>
     </>
   );
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,30 +1,23 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import Navbar from "../components/custom/Navbar";
+import LandingContent from "../lib/LandingPageContent";
+import HeroImg from "../assets/hero-img.png";
 
 const LandingPage = () => {
-  const { isAuthenticated } = useAuth();
-
+  const { isAuthenticated, user } = useAuth();
 
   return (
-    <>
-      <div>LandingPage</div>
-
-      {isAuthenticated ? (
-        <Link to="/dashboard">
-          <button>Go to Dashboard</button>
-        </Link>
-      ) : (
-        <>
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-          <Link to="/register">
-            <button>Register</button>
-          </Link>
-        </>
-      )}
-    </>
+    <div className="bg-[#070707] min-h-screen w-full">
+      <Navbar user={user} />
+      <div className="w-4/5 mx-auto relative">
+        <div className="">
+          <h2>{LandingContent.hero.headline}</h2>
+        </div>
+        <img src={HeroImg} alt="" className="absolute right-[-10%] top-[20%] -z-50" />
+      </div>
+    </div>
   );
 };
 
