df175dbfcc0cfb6135039fef5458a5238352a423
Abhishek Kumar <abhi1322jii@gmail.com>
2025-06-27 12:38:39 +0530

    fix: vite config fix

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 1500, // Increase chunk size limit warning
+  },
+  server: {
+    // Optional: Useful for local dev testing SPA routing
+    historyApiFallback: true,
   },
 });
